7e92b5ce11132f0ea3307f9d7b35ae63b48bd8f5
penny506 <kevin.penny@gmail.com>
2025-02-06 23:38:42 -0700

    Sweet UI with Validation, Difficulty, Stats view buttons with a surprise if you Validate incorrectly!

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { PrimeReactProvider } from 'primereact/api';
 import './index.css';
 import App from './App';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<PrimeReactProvider value={{ unstyled: false }}><App /></PrimeReactProvider>);
